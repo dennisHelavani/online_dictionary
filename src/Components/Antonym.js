f558a7d8dc067646424fb70e4efb6354a311d47d
DennisHelavani <dengelovani@gmail.com>
2022-05-18 16:57:34 +0300

    styles fixed

--- src/Components/Antonym.js
+++ src/Components/Antonym.js
@@ -3,11 +3,10 @@ const Antonym = ({ mean }) => {
     <div className="columns-2 md:columns-2">
       {mean.map((val) =>
         val.meanings.map((means) =>
-          means.antonyms.map((Ant, i) => {
-
+          means.antonyms.map((ant, i) => {
             return (
               <div key={i}>
-                <li className="definition-li">{Ant}</li>
+                <li className="definition-li">{ant}</li>
               </div>
             );
           })
